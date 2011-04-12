563280630b26820f29349a1e4f5b91f21678d800
Michael Schuldt <Michael.Schuldt@bmw-carit.de>
2011-04-12 09:56:32 +0200

    Unit Tests added, License updated

---
+++ LayerManagerPlugins/Communicators/DBUSCommunicator/test/mock_Layerlist.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+*
+* Copyright 2010,2011 BMW Car IT GmbH
+*
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*		http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+****************************************************************************/
+
+#ifndef MockLayerList_H_
+#define MockLayerList_H_
+
+#include <list>
+#include <gmock/gmock.h>  // Brings in Google Mock.
+class MockLayerList : public ILayerList{
+ public:
+  MOCK_METHOD1(createLayer, Layer*(unsigned int));
+  MOCK_METHOD1(createSurface, Surface*(unsigned int));
+  MOCK_METHOD1(createLayerGroup, LayerGroup*(unsigned int));
+  MOCK_METHOD1(createSurfaceGroup, SurfaceGroup*(unsigned int));
+  MOCK_METHOD1(removeLayer, void(Layer*));
+  MOCK_METHOD1(removeSurface, void(Surface*));
+  MOCK_METHOD1(getLayer, Layer*(unsigned int));
+  MOCK_METHOD1(getSurface, Surface*(unsigned int));
+  MOCK_METHOD1(getSurfaceGroup, SurfaceGroup*(unsigned int));
+  MOCK_METHOD1(getLayerGroup, LayerGroup*(unsigned int id));
+  MOCK_METHOD2(getLayerIDs, void(unsigned int*,unsigned int**));
+  MOCK_METHOD3(getLayerIDsOfScreen, bool(unsigned int,unsigned int*,unsigned int**));
+  MOCK_METHOD2(getSurfaceIDs, void(unsigned int*,unsigned int**));
+  MOCK_METHOD2(getLayerGroupIDs, void(unsigned int*,unsigned int**));
+  MOCK_METHOD2(getSurfaceGroupIDs, void(unsigned int*,unsigned int**));
+  MOCK_METHOD0(lockList, void());
+  MOCK_METHOD0(unlockList, void());
+
+
+};
+
+#endif /* MockLayerList_H_ */
