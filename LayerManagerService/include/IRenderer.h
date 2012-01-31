36764ab4b1573b68dc2046b2448b53144183552e
Timo Lotterbach <timo.lotterbach@bmw-carit.de>
2012-01-31 11:59:00 +0100

    LayerManagerService: added documentation for IRenderer
    Adde doxygen documentation to IRenderer interface to the describe the Renderer API in the LayerManagement documentation.

--- LayerManagerService/include/IRenderer.h
+++ LayerManagerService/include/IRenderer.h
@@ -23,47 +23,114 @@
 #include <string>
 #include "LayerType.h"
 #include "Shader.h"
+
 /**
  * Abstract Base of all CompositingControllers, ie Renderers.
+ * \defgroup RendererAPI Layer Management Renderer API
  */
 class IRenderer
 {
 public:
+    /**
+     * \brief      default destructor
+     */
     virtual ~IRenderer()
     {
     }
 
     /**
-     * Start the actual renderingprocess (renderloop)
+     * \brief      Start the actual rendering process (render loop)
+     * \ingroup    RendererAPI
+     * \param[in]  width width of display handled by this renderer
+     * \param[in]  height height of display handled by this renderer
+     * \param[in]  displayName name of display handled by this renderer
+     * \return     TRUE: renderer was started successfully
+     * \return     FALSE: renderer start failed
      */
     virtual bool start(int width, int height, const char* displayName) = 0;
 
     /**
-     * Stop renderingprocess
+     * \brief      Stop rendering process (stop render loop)
+     * \ingroup    RendererAPI
      */
     virtual void stop() = 0;
 
     /**
-     * Switch debug mode of this component on or off
-     * @param onoff Turn on debug mode (true) or off (false)
+     * \brief      Switch debug mode of this component on or off
+     * \ingroup    RendererAPI
+     * \param[in]  onoff TRUE: Turn on debug mode, FALSE: Turn off debug mode
      */
     virtual void setdebug(bool onoff) = 0;
 
+    /**
+     * \brief      Store graphical content of screen to bitmap
+     * \ingroup    RendererAPI
+     * \param[in]  fileToSave path to bitmap file to store the graphical content
+     */
     virtual void doScreenShot(std::string fileToSave) = 0;
+
+    /**
+     * \brief      Store graphical content of layer to bitmap
+     * \ingroup    RendererAPI
+     * \param[in]  fileToSave path to bitmap file to store the graphical content
+     * \param[in]  id id of layer
+     */
     virtual void doScreenShotOfLayer(std::string fileToSave, const unsigned int id) = 0;
+
+    /**
+     * \brief      Store graphical content of surface to bitmap
+     * \ingroup    RendererAPI
+     * \param[in]  fileToSave path to bitmap file to store the graphical content
+     * \param[in]  id id of surface
+     */
     virtual void doScreenShotOfSurface(std::string fileToSave, const unsigned int id, const unsigned int layer_id) = 0;
 
+    /**
+     * \brief      Get the capabilies of a layer type
+     * \ingroup    RendererAPI
+     * \param[in]  layertype type of layer
+     * \return     bitset with flags set for capabilities
+     */
     virtual unsigned int getLayerTypeCapabilities(LayerType layertype) = 0;
 
+    /**
+     * \brief      Get the number of supported hardware layers of the renderer for a screen
+     * \ingroup    RendererAPI
+     * \param[in]  screenID id of the screen
+     * \return     Number of supported hardware layers for screen
+     */
     virtual unsigned int getNumberOfHardwareLayers(unsigned int screenID) = 0;
 
+    /**
+     * \brief      Get the resolution of a screen handled by this renderer
+     * \ingroup    RendererAPI
+     * \param[in]  screenID id of the screen
+     * \return     array with width and height of screen
+     */
     virtual unsigned int* getScreenResolution(unsigned int screenID) = 0;
 
+    /**
+     * \brief      Get the list if available screen ids
+     * \ingroup    RendererAPI
+     * \param[out] length length of the returned array
+     * \return     array containing all available screen ids
+     */
     virtual unsigned int* getScreenIDs(unsigned int* length) = 0;
-    
+
+    /**
+     * \brief      Create a shader object (that can be applied to surfaces)
+     * \ingroup    RendererAPI
+     * \param[in]  vertexName filename of vertex shader source code
+     * \param[in]  fragmentName filename of fragment shader source code
+     * \return     Pointer to created shader object
+     */
     virtual Shader* createShader(const string* vertexName, const string* fragmentName) = 0;
 
-    virtual void signalWindowSystemRedraw()=0;
+    /**
+     * \brief      Trigger a redraw for this renderer
+     * \ingroup    RendererAPI
+     */
+    virtual void signalWindowSystemRedraw() = 0;
 };
 
 #endif /* _IRENDERER_H_ */
